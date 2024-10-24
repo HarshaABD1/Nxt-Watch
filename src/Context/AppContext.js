83839fd8289a47c1390e10d8ef9b3a81ed7c095b
HarshaABD1 <harshareddyabd@gmail.com>
2024-10-24 08:58:50 +0000

    Nxt Watch

---
+++ src/Context/AppContext.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const AppContext = React.createContext({
+  lightTheme: true,
+  changeTheme: () => {},
+  savedList: [],
+  updateSavedList: () => {},
+  removeVideo: () => {},
+})
+
+export default AppContext
